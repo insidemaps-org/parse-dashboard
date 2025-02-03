1ae56117b073dee3e99e5f1f2473baf8dff7e222
Djole <n.dj2503@gmail.com>
2025-02-03 17:53:35 +0100

    Disabling sort for all fields and tables

--- src/components/DataBrowserHeaderBar/DataBrowserHeaderBar.react.js
+++ src/components/DataBrowserHeaderBar/DataBrowserHeaderBar.react.js
@@ -35,7 +35,7 @@ export default class DataBrowserHeaderBar extends React.Component {
       </div>,
     ];
 
-    headers.forEach(({ width, name, type, targetClass, order, visible, preventSort }, i) => {
+    headers.forEach(({ width, name, type, targetClass, order, visible, _preventSort }, i) => {
       if (!visible) {
         return;
       }
@@ -46,21 +46,24 @@ export default class DataBrowserHeaderBar extends React.Component {
         wrapStyle.background = '#66637A';
       }
       let onClick = null;
-      if (
-        !preventSort &&
-        (type === 'String' || type === 'Number' || type === 'Date' || type === 'Boolean')
-      ) {
-        onClick = () =>{
-          updateOrdering((order === 'descending' ? '' : '-') + name);
-          setSelectedObjectId(null);
-          setCurrent(null)
-        }
-      }
+      // do not allow sort of any kind...
+      // sort requires indexes to be properly set
+      // should be refactored to whitelist instead of blacklist
+      // if (
+      //   !preventSort &&
+      //   (type === 'String' || type === 'Number' || type === 'Date' || type === 'Boolean')
+      // ) {
+      //   onClick = () =>{
+      //     updateOrdering((order === 'descending' ? '' : '-') + name);
+      //     setSelectedObjectId(null);
+      //     setCurrent(null)
+      //   }
+      // }
 
       let className = styles.wrap;
-      if (preventSort) {
+      // if (preventSort) {
         className += ` ${styles.preventSort} `;
-      }
+      // }
 
       elements.push(
         <div onClick={onClick} key={'header' + i} className={className} style={wrapStyle}>
